56764dc1d73f9d82251e71e108dfb4f7b8718072
0xchamin <chmk90@gmail.com>
2019-01-01 23:46:14 +0800

    added hi.c

---
+++ prerequisites/C/hi.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    /*
+     variable declarations must come before other types of statements in the given "code block"
+    */
+    printf("I am alive! Beware. \n");
+    getchar(); //reads a single character and waits for user to hit enter. 
+    return 0;
+}
