88a29e550d7cb9c00441fcbecfb915df926ed8ac
0xchamin <chmk90@gmail.com>
2019-01-20 14:42:05 +0800

    cpp initial commit

---
+++ prerequisites/C++/hello.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    printf("Hello CK! \n");
+    return 0;
+    //compile and run 
+    // gcc -o test hello.cpp 
+}
---
+++ prerequisites/C++/hw.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,sum;
+    cout<<"Enter 2 numbers";
+    cin >> a >> b;
+    sum = a+b;
+    cout<<"sum is = "<<sum;
+    return 0;
+    // g++ -o test hw.cpp 
+}
